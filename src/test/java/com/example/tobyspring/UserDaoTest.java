8a7a0311972796f9afeade90d136cc7242fd3638
hj-k66 <hjx66@naver.com>
2024-02-20 21:03:01 +0900

    test: getCount() 테스트 및 addAndGet() 테스트 보완

--- src/test/java/com/example/tobyspring/UserDaoTest.java
+++ src/test/java/com/example/tobyspring/UserDaoTest.java
@@ -22,11 +22,34 @@ public class UserDaoTest {
         user.setId("hihi");
         user.setName("김희정");
         user.setPassword("1234");
+        User user2 = new User("h2", "김희정2", "pass");
 
         userDao.add(user);
+        userDao.add(user2);
+        assertThat(userDao.getCount()).isEqualTo(2);
+
+        User finded = userDao.get(user.getId());
+        assertThat(finded.getName()).isEqualTo(user.getName());
+        User finded2 = userDao.get(user2.getId());
+        assertThat(finded2.getName()).isEqualTo(user2.getName());
+    }
+
+    @Test
+    void getCount() throws SQLException{
+        User user1 = new User("h1", "김희정", "1234");
+        User user2 = new User("h2", "김희정2", "pass");
+        User user3 = new User("h3", "김희정3", "word");
+
+        userDao.deleteAll();
+        assertThat(userDao.getCount()).isEqualTo(0);
+
+        userDao.add(user1);
         assertThat(userDao.getCount()).isEqualTo(1);
 
-        User finded = userDao.get("hihi");
-        assertThat(finded.getId()).isEqualTo(user.getId());
+        userDao.add(user2);
+        assertThat(userDao.getCount()).isEqualTo(2);
+
+        userDao.add(user3);
+        assertThat(userDao.getCount()).isEqualTo(3);
     }
 }
