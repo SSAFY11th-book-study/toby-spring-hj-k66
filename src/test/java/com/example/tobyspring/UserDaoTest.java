642d8a250abbca74c2df9616bbb34782911cd5ac
hj-k66 <hjx66@naver.com>
2024-02-20 21:32:13 +0900

    refactor: @BeforeEach로 중복되는 테스트 코드 리팩토링

--- src/test/java/com/example/tobyspring/UserDaoTest.java
+++ src/test/java/com/example/tobyspring/UserDaoTest.java
@@ -8,40 +8,43 @@ import com.example.tobyspring.dao.DaoFactory;
 import com.example.tobyspring.dao.UserDao;
 import com.example.tobyspring.domain.User;
 import java.sql.SQLException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.dao.EmptyResultDataAccessException;
 
 public class UserDaoTest {
 
     private final UserDao userDao = new DaoFactory().userDao();
+    private User user1;
+    private User user2;
+    private User user3;
 
+    @BeforeEach
+    void setUp(){
+        user1 = new User("h1", "김희정", "1234");
+        user2 = new User("h2", "김희정2", "pass");
+        user3 = new User("h3", "김희정3", "word");
+
+    }
+
+    // Junit 5부터는 접근제한자가 Default 여도 된다. (JUnit4 까지는 public이어야 했링)
     @Test
     void addAndGet() throws SQLException {
         userDao.deleteAll();
         assertThat(userDao.getCount()).isEqualTo(0);
 
-        User user = new User();
-        user.setId("hihi");
-        user.setName("김희정");
-        user.setPassword("1234");
-        User user2 = new User("h2", "김희정2", "pass");
-
-        userDao.add(user);
+        userDao.add(user1);
         userDao.add(user2);
         assertThat(userDao.getCount()).isEqualTo(2);
 
-        User finded = userDao.get(user.getId());
-        assertThat(finded.getName()).isEqualTo(user.getName());
+        User finded = userDao.get(user1.getId());
+        assertThat(finded.getName()).isEqualTo(user1.getName());
         User finded2 = userDao.get(user2.getId());
         assertThat(finded2.getName()).isEqualTo(user2.getName());
     }
 
     @Test
     void getCount() throws SQLException {
-        User user1 = new User("h1", "김희정", "1234");
-        User user2 = new User("h2", "김희정2", "pass");
-        User user3 = new User("h3", "김희정3", "word");
-
         userDao.deleteAll();
         assertThat(userDao.getCount()).isEqualTo(0);
 
