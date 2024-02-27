22472245ef40d10bee457b0c0e6813b1e7160fb4
heejung Kim <hjx66@naver.com>
2024-02-27 23:36:36 +0900

    [Chapter2] 테스트 (#2)
    * feat: deleteAll(), getCount() 추가 및 테스트
    
    * test: getCount() 테스트 및 addAndGet() 테스트 보완
    
    * feat: get() 조회하려는 id 없는 경우 예외처리
    
    * refactor: @BeforeEach로 중복되는 테스트 코드 리팩토링
    
    * test: @Autowired로 DI
    
    * test: Junit 테스트 메소드 수행 시 매번 새로운 오브젝트 생성 테스트
    
    * test: 같은 테스트 컨텍스트 공유하는지 확인

--- src/main/java/com/example/tobyspring/dao/UserDao.java
+++ src/main/java/com/example/tobyspring/dao/UserDao.java
@@ -6,12 +6,39 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.sql.DataSource;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 public class UserDao {
     private DataSource dataSource;
 
     public UserDao(DataSource dataSource){
         this.dataSource = dataSource;
+    }
+
+    public void deleteAll() throws SQLException{
+        Connection c = dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement("delete from users");
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public int getCount() throws SQLException{
+        Connection c = dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement("select count(*) from users");
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        int count = rs.getInt(1);
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return count;
     }
     public void add(User user) throws SQLException {
         //1. DB 연결을 위한 Connection 가져온다.
@@ -46,17 +73,21 @@ public class UserDao {
         //3. 만들어진 statement를 실행
         //4. 조회의 경우, SQL 쿼리 실행 결과를 ResultSet으로 받아서 정보를 저장할 오브젝트(user)에 옮겨준다.
         ResultSet rs = ps.executeQuery();
-        rs.next();
-        User user = new User();
-        user.setId(rs.getString("id"));
-        user.setName(rs.getString("name"));
-        user.setPassword(rs.getString("password"));
+        User user = null;
+
+        if(rs.next()){
+            user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+        }
+
 
         //5. 작업 중 생성된 Connection, Statement 등 리소스는 작업을 마친 후 반드시 닫는다.
         rs.close();
         ps.close();
         c.close();
-
+        if(user == null) throw new EmptyResultDataAccessException(0);
         return user;
     }
 }
--- src/test/java/com/example/tobyspring/UserDaoTest.java
+++ src/test/java/com/example/tobyspring/UserDaoTest.java
@@ -2,26 +2,73 @@ package com.example.tobyspring;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.example.tobyspring.dao.DaoFactory;
 import com.example.tobyspring.dao.UserDao;
 import com.example.tobyspring.domain.User;
 import java.sql.SQLException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)  //JUnit5로 넘어오면서 @RunWith는 @ExtendWith로 변환
+@ContextConfiguration(classes = DaoFactory.class)
 public class UserDaoTest {
 
-    private final UserDao userDao = new DaoFactory().userDao();
+    @Autowired
+    private UserDao userDao;
+    private User user1;
+    private User user2;
+    private User user3;
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        user1 = new User("h1", "김희정", "1234");
+        user2 = new User("h2", "김희정2", "pass");
+        user3 = new User("h3", "김희정3", "word");
+        System.out.println(userDao);
+        userDao.deleteAll();
+        assertThat(userDao.getCount()).isEqualTo(0);
+
+    }
+
+    // Junit 5부터는 접근제한자가 Default 여도 된다. (JUnit4 까지는 public이어야 했링)
     @Test
-    void findById() throws SQLException, ClassNotFoundException {
-        User user = new User();
-        user.setId("hihi");
-        user.setName("김희정");
-        user.setPassword("1234");
-        userDao.add(user);
+    void addAndGet() throws SQLException {
+        userDao.add(user1);
+        userDao.add(user2);
+        assertThat(userDao.getCount()).isEqualTo(2);
 
-        User finded = userDao.get("hihi");
+        User finded = userDao.get(user1.getId());
+        assertThat(finded.getName()).isEqualTo(user1.getName());
+        User finded2 = userDao.get(user2.getId());
+        assertThat(finded2.getName()).isEqualTo(user2.getName());
+    }
 
-        assertThat(finded.getId()).isEqualTo(user.getId());
+    @Test
+    void getCount() throws SQLException {
+        userDao.add(user1);
+        assertThat(userDao.getCount()).isEqualTo(1);
+
+        userDao.add(user2);
+        assertThat(userDao.getCount()).isEqualTo(2);
+
+        userDao.add(user3);
+        assertThat(userDao.getCount()).isEqualTo(3);
+    }
+
+    //    @Test(expected= EmptyResultDataAccessException.class)
+    /*
+    Junit5부터 @Test annotation expected 속성을 사용하여 예외 값 지정 불가능.
+    assertThrows메서드를 사용하여 예상되는 예외의 유형과 람다 식을 통해 테스트 중인 코드를 전달할 수 있는 실행 가능한 기능 인터페이스를 사용.
+     */
+    @Test
+    void getUserFail() throws SQLException {
+        assertThrows(EmptyResultDataAccessException.class, () -> userDao.get("fail_id"));
     }
 }
