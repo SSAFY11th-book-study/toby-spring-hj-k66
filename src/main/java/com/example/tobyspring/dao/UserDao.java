953cef318c09747c94d5e28c1f135cf141c87121
hj-k66 <hjx66@naver.com>
2024-03-04 19:40:58 +0900

    feat: deleteAll(), getCount() 예외처리

--- src/main/java/com/example/tobyspring/dao/UserDao.java
+++ src/main/java/com/example/tobyspring/dao/UserDao.java
@@ -11,35 +11,76 @@ import org.springframework.dao.EmptyResultDataAccessException;
 public class UserDao {
     private DataSource dataSource;
 
-    public UserDao(DataSource dataSource){
+    public UserDao(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
-    public void deleteAll() throws SQLException{
-        Connection c = dataSource.getConnection();
+    public void deleteAll() throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
 
-        PreparedStatement ps = c.prepareStatement("delete from users");
-        ps.executeUpdate();
+        try {
+            c = dataSource.getConnection();
+            ps = c.prepareStatement("delete from users");
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
 
-        ps.close();
-        c.close();
+                }
+            }
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+
+                }
+            }
+        }
     }
 
-    public int getCount() throws SQLException{
-        Connection c = dataSource.getConnection();
+    public int getCount() throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
 
-        PreparedStatement ps = c.prepareStatement("select count(*) from users");
+        try {
+            c = dataSource.getConnection();
+            ps = c.prepareStatement("select count(*) from users");
+            rs = ps.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
 
-        ResultSet rs = ps.executeQuery();
-        rs.next();
-        int count = rs.getInt(1);
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
 
-        rs.close();
-        ps.close();
-        c.close();
+                }
+            }
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
 
-        return count;
+                }
+            }
+        }
     }
+
     public void add(User user) throws SQLException {
         //1. DB 연결을 위한 Connection 가져온다.
         Connection c = dataSource.getConnection();
@@ -48,7 +89,7 @@ public class UserDao {
         PreparedStatement ps = c.prepareStatement(
                 "insert into users(id,name,password) values(?,?,?)"
         );
-        ps.setString(1,user.getId());
+        ps.setString(1, user.getId());
         ps.setString(2, user.getName());
         ps.setString(3, user.getPassword());
 
@@ -68,26 +109,27 @@ public class UserDao {
         PreparedStatement ps = c.prepareStatement(
                 "select * from users where id = ?"
         );
-        ps.setString(1,id);
+        ps.setString(1, id);
 
         //3. 만들어진 statement를 실행
         //4. 조회의 경우, SQL 쿼리 실행 결과를 ResultSet으로 받아서 정보를 저장할 오브젝트(user)에 옮겨준다.
         ResultSet rs = ps.executeQuery();
         User user = null;
 
-        if(rs.next()){
+        if (rs.next()) {
             user = new User();
             user.setId(rs.getString("id"));
             user.setName(rs.getString("name"));
             user.setPassword(rs.getString("password"));
         }
 
-
         //5. 작업 중 생성된 Connection, Statement 등 리소스는 작업을 마친 후 반드시 닫는다.
         rs.close();
         ps.close();
         c.close();
-        if(user == null) throw new EmptyResultDataAccessException(0);
+        if (user == null) {
+            throw new EmptyResultDataAccessException(0);
+        }
         return user;
     }
 }
